d4504d2fc0de336b8cbdf61981d45c18975eddb5
Mohammed Maaz <mohammedmaaz2623@gmail.com>
2023-05-17 15:25:28 +0530

    modified:   server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import express from "express";
 const app = express()
 
 app.use('/', (req, res) => {
-    res.json("Server Running")
+    res.json({message: "Server Running"})
 })
 
 app.listen(5000, () => console.log("Server running on port 5000"))
