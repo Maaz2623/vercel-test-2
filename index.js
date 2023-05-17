6bc871b88f4ec898df7c52ed0e005c5cc6532ba4
Mohammed Maaz <mohammedmaaz2623@gmail.com>
2023-05-17 18:26:07 +0530

    modified:   index.js

--- index.js
+++ index.js
@@ -2,12 +2,9 @@ import express from "express";
 
 const app = express()
 
-app.use('/', (req, res) => {
-    res.json({message: "Server Running"})
-})
 
-app.use('/users', (req, res) => {
-    res.send("Server Running")
+app.use('/', (req, res) => {
+    res.send("Server Running Successfully")
 })
 
 app.listen(5000, () => console.log("Server running on port 5000"))
