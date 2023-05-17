86708af4d2987438469f0037f3cbbab3818ee3cb
Mohammed Maaz <mohammedmaaz2623@gmail.com>
2023-05-17 15:09:49 +0530

    new file:   .gitignore 	new file:   package-lock.json 	new file:   package.json 	new file:   server.js

---
+++ server.js
@@ -0,0 +1,9 @@
+import express from "express";
+
+const app = express()
+
+app.use('/', (req, res) => {
+    res.send("Server Running")
+})
+
+app.listen(5000, () => console.log("Server running on port 5000"))
